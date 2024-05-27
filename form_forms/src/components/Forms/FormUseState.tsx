7d2dcf5a42403d139ddf7f3d802e132cf17bfc8c
Raquelbateman <aliciaraquel1017@gmail.com>
2024-05-27 10:58:19 -0700

    update

--- form_forms/src/components/Forms/FormUseState.tsx
+++ form_forms/src/components/Forms/FormUseState.tsx
@@ -1,17 +1,15 @@
-
-
 const FormUseState = () => {
   return (
     <>
-    <h1 className="text-center">Form Using UseState</h1>
-    <form >
-
-
-        
-    </form>
-    
+      <h1 className="text-center">Form Using UseState</h1>
+      <form>
+        <div className="mb-3 myContainer">
+          <label htmlFor="" className="form-label">Name</label>
+          <input type="text" className="form-control" />
+        </div>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default FormUseState
+export default FormUseState;
