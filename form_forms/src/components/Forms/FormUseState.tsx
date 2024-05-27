8ad72eda9a01300890d1f15d96f509c3c2e1a930
Raquelbateman <aliciaraquel1017@gmail.com>
2024-05-27 11:20:56 -0700

    update

--- form_forms/src/components/Forms/FormUseState.tsx
+++ form_forms/src/components/Forms/FormUseState.tsx
@@ -1,28 +1,40 @@
 const FormUseState = () => {
   return (
     <>
-      <h1 className="text-center">Form Using UseState</h1>
+      <h1 className="text-center">Registration Form</h1>
       <form>
         <div className="mb-3 myContainer">
-            <div className="row">
-                <div className="col">
-          <label htmlFor="" className="form-label">First Name</label>
-          <input type="text" id="firstName" className="form-control" />
+          <div className="row">
+            <div className="col">
+              <label htmlFor="" className="form-label">
+                First Name
+              </label>
+              <input type="text" id="firstName" className="form-control" />
+            </div>
+            <div className="col">
+              <label htmlFor="" className="form-label">
+                Last Name
+              </label>
+              <input type="text" id="lastName" className="form-control" />
+            </div>
           </div>
-           <div className="col">
-          <label htmlFor="" className="form-label">Last Name</label>
-          <input type="text" id="lastName" className="form-control" />
-          </div>
-          </div>
-          <label htmlFor="" className="form-label">Email</label>
+          <label htmlFor="" className="form-label">
+            Email
+          </label>
           <input type="text" id="email" className="form-control" />
-          <label htmlFor="" className="form-label">Password</label>
+          <label htmlFor="" className="form-label">
+            Password
+          </label>
           <input type="text" id="password" className="form-control" />
-          <label htmlFor="" className="form-label"> Confirm Password</label>
+          <label htmlFor="" className="form-label">
+            {" "}
+            Confirm Password
+          </label>
           <input type="text" id="confirmPassword" className="form-control" />
-          <button className="mt-3 btn btn-primary" type='submit'>Submit</button>
+          <button className="mt-3 btn btn-primary" type="submit">
+            Submit
+          </button>
         </div>
-       
       </form>
     </>
   );
