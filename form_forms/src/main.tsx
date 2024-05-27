a2b714e0a5c9544291835354ef69be429c9177c9
Raquelbateman <aliciaraquel1017@gmail.com>
2024-05-27 10:24:05 -0700

    update

--- form_forms/src/main.tsx
+++ form_forms/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import 'bootstrap/dist/css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
