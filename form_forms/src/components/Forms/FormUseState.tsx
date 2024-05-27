f77fd9d0d5d4ef24f31ed8cae4e8fc11d03c161a
Raquelbateman <aliciaraquel1017@gmail.com>
2024-05-27 11:07:27 -0700

    update

--- form_forms/src/components/Forms/FormUseState.tsx
+++ form_forms/src/components/Forms/FormUseState.tsx
@@ -5,8 +5,12 @@ const FormUseState = () => {
       <form>
         <div className="mb-3 myContainer">
           <label htmlFor="" className="form-label">Name</label>
-          <input type="text" className="form-control" />
+          <input type="text" id="name" className="form-control" />
+          <label htmlFor="" className="form-label">Age</label>
+          <input type="number" id="age" className="form-control" />
+          <button className="mt-3 btn btn-primary" type='submit'>Submit</button>
         </div>
+       
       </form>
     </>
   );
